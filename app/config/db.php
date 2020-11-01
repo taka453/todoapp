b34466d8be0200f58721db0649e4fe0a3c70c12d
taka453 <paisley.park.39@gmail.com>
2020-11-01 21:16:02 +0900

    configディレクトリを作成し、dbにDB接続情報を記入

---
+++ app/config/db.php
@@ -0,0 +1,8 @@
+<?php
+
+//DB接続の記述
+//hostはmysqlコンテナに入って記述を確認する必要あり。
+//定数に変更、定数は大文字で記入、値の再代入ができないので注意
+const DSN='mysql:host=c77868a2fbb8;dbname=common;charset=utf8';
+const USERNAME ='takano';
+const PASSWORD ='takano';
--- app/view/todo/index.php
+++ app/view/todo/index.php
@@ -1,13 +1,10 @@
 <?php
 
-//DB接続の記述
-//hostはmysqlコンテナに入って記述を確認する必要あり。
-$dsn='mysql:host=c77868a2fbb8;dbname=common;charset=utf8';
-$user='takano';
-$password='takano';
+// require_onceでDB接続情報を呼び出す
+require_once ("./../../config/db.php");
 
 //PDOクラスで記入,コンストラクタに対して$dsn,$user,$password情報を渡す
-$pdo = new PDO($dsn, $user, $password);
+$pdo = new PDO(DSN, USERNAME, PASSWORD);
 
 //queryメソッドの引数にsql文を足す事によりsqlを実行できる
 //返り値は連想配列ではかえってこない
@@ -23,6 +20,8 @@ $stmh = $pdo->query("select * from todos");
 //FECH_ASSOCは定数。結果を連想配列で返す指定をしている。
 $todo_list = $stmh->fetchAll(PDO::FETCH_ASSOC);
 
+$todo_list = array();
+
 ?>
 
 <!DOCTYPE html>
@@ -33,12 +32,17 @@ $todo_list = $stmh->fetchAll(PDO::FETCH_ASSOC);
     <title>Document</title>
 </head>
 <body>
-    <!-- foreachでループで取得する、fetchAllにより連想配列にて返されている -->
-    <ul>
-        <?php foreach($todo_list as $todo): ?>
-            <li><?php echo $todo["title"]; ?></li>
-        <?php endforeach; ?>
-    </ul>
+    <!-- データの条件分岐を記入 -->
+    <?php if($todo_list): ?>
+        <!-- foreachでループで取得する、fetchAllにより連想配列にて返されている -->
+        <ul>
+            <?php foreach($todo_list as $todo): ?>
+                <li><?php echo $todo["title"]; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php else: ?>
+        <p>データなし</p>
+    <?php endif; ?>
 </body>
 </html>
 
